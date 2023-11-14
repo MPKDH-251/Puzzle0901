7f1f57c9ecae76a147a547a029c7cca55920375e
MPKDH-251 <150802586+MPKDH-251@users.noreply.github.com>
2023-11-14 23:42:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+const correctAnswer = "wrong"; // Replace with the actual answer
+
+function checkAnswer() {
+    const userAnswer = document.getElementById("answer").value.toLowerCase();
+
+    if (userAnswer === correctAnswer) {
+        alert("Congratulations! You solved the riddle. Please go towards the robo and find your next clue!! ");
+    } else {
+        alert("Oops! Try again.");
+    }
+}
